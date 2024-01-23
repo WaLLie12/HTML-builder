b241e987a83120cacc14d7bb5b62b452db9922b9
vragno01 <vadimserapionov@gmail.com>
2024-01-23 20:37:57 +0200

    feat: 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,25 @@
 const fs = require('fs');
 const path = require('path');
+const createFolder = path.resolve(__dirname, 'text.text')
 
-const text2 = fs.createWriteStream(path.resolve(__dirname, 'text.text'));
+const text2 = fs.createWriteStream(createFolder);
 process.stdout.write('Good day. Please, enter the text right below\n');
 process.stdin.on('data', function (data) {
-  data = data.toString().toLowerCase();
-  text2.write(data);
-  if (data.trim() === 'exit') {
+  if (data.toString().replace(/\r?\n|\r/g, '') === 'exit') {
     leaveSoon();
   }
+  
+  fs.appendFile(createFolder, data.toString(), (error) => {
+    if (error) {
+      console.log(error.message);
+    }
+  })
+  
 });
 
 process.on('SIGINT', leaveSoon);
 
 function leaveSoon() {
   process.stdout.write('Leave soon? bye');
-  setTimeout(() => {
     process.exit();
-  }, 500);
 }
