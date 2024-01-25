33fe7043212fb2542178cce11d8674ce402e16e5
vragno01 <vadimserapionov@gmail.com>
2024-01-25 13:25:35 +0200

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,36 @@
 const fs = require('fs/promises');
 const path = require('path');
+
 const storeDir = path.join(__dirname, 'files');
 const pasteDir = path.join(__dirname, 'files-copy');
 
 async function duplicateDir() {
   try {
     await fs.mkdir(pasteDir, { recursive: true });
-    const files = await fs.readdir(storeDir);
 
-    for (const file of files) {
-      const sourcePath = path.join(storeDir, file);
-      const pasteWay = path.join(pasteDir, file);
+    const sourceFiles = await fs.readdir(storeDir);
+    const destFiles = await fs.readdir(pasteDir);
 
-      const fileContent = await fs.readFile(sourcePath);
-
-      await fs.writeFile(pasteWay, fileContent);
+    const filesToDelete = destFiles.filter(file => !sourceFiles.includes(file));
+    for (const fileToDelete of filesToDelete) {
+      const deletePath = path.join(pasteDir, fileToDelete);
+      await fs.unlink(deletePath);
     }
 
-    console.log('The folder is definitely copied! Check it out!');
+    for (const file of sourceFiles) {
+      const sourcePath = path.join(storeDir, file);
+      const destPath = path.join(pasteDir, file);
+
+      const sourceStats = await fs.stat(sourcePath);
+      const destStats = destFiles.includes(file) ? await fs.stat(destPath) : null;
+
+      if (!destStats || sourceStats.mtimeMs > destStats.mtimeMs) {
+        const fileContent = await fs.readFile(sourcePath);
+        await fs.writeFile(destPath, fileContent);
+      }
+    }
+
+    console.log('The folder is definitely updated! Check it out!');
   } catch (error) {
     console.error(error.message);
   }
